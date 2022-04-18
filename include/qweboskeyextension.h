77f735d0626e5051df09d61a8858cc5a99c65aca
jihee.moon <jihee.moon@lge.com>
2022-04-18 16:39:17 +0900

    Remove unnecessary keycode of game launcher
    :Detailed Notes:
    Remove unnecessary keycode of game launcher
    
    Depends on
    https://wall.lge.com/c/starfish/qt5-qpa-starfish/+/367581
    https://wall.lge.com/c/starfish/libim/+/367580
    https://wall.lge.com/c/webos-pro/qt-features-webos/+/367582
    https://wall.lge.com/c/webos-pro/xkeyboard-config/+/367583
    
    :Testing Performed:
    [TVDEVTC-85] Verify new qt keycode
    
    :Issues Addressed:
    [TVPLAT-196638]
    [WRP-4241]
    
    Change-Id: I2a73ac58e318d1454b50449658a9b3d9e20b5019
    Originally-reviewed-on: https://wall.lge.com/c/webos-pro/qt-features-webos/+/367582
    Cherry-picked-from-commit: 36df0707b398df0ffdfb2ff3007c5d86f6bd1676
    Cherry-picked-from-branch: @40.jcl4tv

--- include/qweboskeyextension.h
+++ include/qweboskeyextension.h
@@ -207,7 +207,6 @@
         Key_webOS_4K_CS_12          = 0x0120016c, \
         /* For Gaming TV */ \
         Key_webOS_Bendable          = 0x0120016d, \
-        Key_webOS_Game_Launcher     = 0x0120016e, \
         /* For B&O remote control */ \
         Key_webOS_BlueLong          = 0x01200171, \
         /* It is not a physical button, but acts like a key */ \
