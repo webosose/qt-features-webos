3850ce74350562839c495c4fe5781601525f2d84
youngnan.kim <youngnan.kim@lge.com>
2022-12-01 12:17:10 +0900

    Fix error for Add CP keycode for Japan
    :Release Notes:
    Fix error for Add CP keycode for Japan
    
    :Detailed Notes:
    Fix error for 'Add CP keycode for Japan'
    depends on
    https://wall.lge.com/c/webos-pro/xkeyboard-config/+/397594
    https://wall.lge.com/c/webos-pro/qt-features-webos/+/397569
    https://wall.lge.com/c/starfish/qt5-qpa-starfish/+/397565
    https://wall.lge.com/c/starfish/starfish-luna-surface-manager/+/397563
    https://wall.lge.com/c/starfish/libim/+/397562
    
    :Testing Performed:
    [TVDEVTC-85] Verify new qt keycode
    
    :QA Notes:
    
    :Issues Addressed:
    [TVPLAT-248088]
    [WRP-9865]
    
    Change-Id: I01a371aa7e335477c3fd602e066538cd4c46b699
    Originally-reviewed-on: https://wall.lge.com/c/webos-pro/qt-features-webos/+/401376
    Cherry-picked-from-commit: 031eb8db2cb6b18f29ed5c2e4d95b8a0aa8acfa5
    Cherry-picked-from-branch: @40.jcl4tv

--- include/qweboskeyextension.h
+++ include/qweboskeyextension.h
@@ -216,7 +216,7 @@
         Key_webOS_UsbHeadsetMute    = 0x0120016e, \
         /* For B&O remote control */ \
         Key_webOS_BlueLong          = 0x01200171, \
-        /* For CP Key for Japan */
+        /* For CP Key for Japan */ \
         Key_webOS_Hulu              = 0x01200172, \
         Key_webOS_Nhkplus           = 0x01200173, \
         Key_webOS_Tod               = 0x01200174, \
